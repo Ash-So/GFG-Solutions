1c9ee44224a730f75e68291c9d428b48dfcbb62e
Ash-So <142682875+Ash-So@users.noreply.github.com>
2023-09-26 09:42:21 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Print Matrix in snake Pattern/print-matrix-in-snake-pattern.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to return list of integers visited in snake pattern in matrix.
+    vector<int> snakePattern(vector<vector<int> > matrix)
+    {   
+        vector<int> x;
+        
+        
+        for(int i=1;i<matrix.size();i++){
+            if(i%2!=0){
+                reverse(matrix[i].begin(),matrix[i].end());  
+            }
+        }
+        
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                x.push_back(matrix[i][j]);
+            }
+        }
+        
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.snakePattern(matrix);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
